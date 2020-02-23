1567a07f98e876ecd8795b451564fef471999c78
luv2144 <56426163+luv2144@users.noreply.github.com>
2020-02-24 01:29:08 +0300

    refactor task1

--- AutomationPractice/src/test/java/CheckEmailHappyPath.java
+++ AutomationPractice/src/test/java/CheckEmailHappyPath.java
@@ -5,9 +5,8 @@ import org.testng.annotations.Test;
 public class CheckEmailHappyPath {
 
     @DataProvider(name = "validEmail")
-
-    public static Object[] credentials() {
-        return new Object[] {"vu_@me.cm", "p!AZXGVFJDvdjfshdMOD@z79aA.abcdz", "_098EsazAZ@a8nz.com"};
+    public static Object[][] credentials() {
+        return new Object[][] {{"vu_@me.cm"}, {"p!AZXGVFJDvdjfshdMOD@z79aA.abcdz"}, {"_098EsazAZ@a8nz.com"}};
     }
 
     @Test(dataProvider = "validEmail")
