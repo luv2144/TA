20a0c09ee3a54333dece7fa9a598655e9dc1d7ae
luv2144 <lthreeblog@gmail.com>
2020-02-29 16:43:19 +0300

    hw2

--- AutomationPractice/src/test/java/seleniumBaseApi/TestDropdown.java
+++ AutomationPractice/src/test/java/seleniumBaseApi/TestDropdown.java
@@ -3,7 +3,6 @@ package seleniumBaseApi;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -11,13 +10,24 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class TestDropdown {
-    private WebDriver chromeDriver = new ChromeDriver();
-    private WebDriverWait wait = new WebDriverWait(chromeDriver, 5);
+    WebDriver chromeDriver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(chromeDriver, 10);
+
+    @DataProvider(name = "expectedLinks")
+    public static Object[][] verifyLinks() {
+        return new Object[][]{{1, "https://formy-project.herokuapp.com/autocomplete"},
+                {2, "https://formy-project.herokuapp.com/buttons"},
+                {3, "https://formy-project.herokuapp.com/checkbox"},
+                {4, "https://formy-project.herokuapp.com/datepicker"},
+                {5, "https://formy-project.herokuapp.com/dragdrop"},
+                {6, "https://formy-project.herokuapp.com/dropdown"},
+                {7, "https://formy-project.herokuapp.com/enabled"},
+                {8, "https://formy-project.herokuapp.com/fileupload"}
+        };
+    }
 
     @BeforeClass
     public void navigateToDropdownPage() {
@@ -25,42 +35,16 @@ public class TestDropdown {
         chromeDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void selectDropdownLinks() {
-        String[] expectedLinks = {"https://formy-project.herokuapp.com/autocomplete",
-                "https://formy-project.herokuapp.com/buttons",
-                "https://formy-project.herokuapp.com/checkbox",
-                "https://formy-project.herokuapp.com/datepicker",
-                "https://formy-project.herokuapp.com/dragdrop",
-                "https://formy-project.herokuapp.com/dropdown",
-                "https://formy-project.herokuapp.com/enabled",
-                "https://formy-project.herokuapp.com/fileupload"
-        };
-
-//        for (int i = 0; i <= 7; i++) {
-//            wait.until(ExpectedConditions.elementToBeClickable(By.id("dropdownMenuButton")));
-            chromeDriver.findElement(By.id("dropdownMenuButton")).click();
-//            chromeDriver.findElement(By.cssSelector("#dropdownMenuButton + div a:nth-child(" + (i+1) + ")"));
-//            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + (1) + "]")));
-            chromeDriver.findElement(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + (1) + "]")).click();
-            String actualResult = chromeDriver.getCurrentUrl();
-            String expectedResult = expectedLinks[0];
-            Assert.assertEquals(actualResult, expectedResult);
-            chromeDriver.navigate().back();
-//        }
-
+    @Test(dataProvider = "expectedLinks")
+    public void selectDropdownLinks(int index, String expectedLink) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("dropdownMenuButton")));
         chromeDriver.findElement(By.id("dropdownMenuButton")).click();
-        chromeDriver.findElement(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + (2) + "]")).click();
-        String actualResult2 = chromeDriver.getCurrentUrl();
-        String expectedResult2 = expectedLinks[1];
-        Assert.assertEquals(actualResult2, expectedResult2);
-        chromeDriver.navigate().back();
-
-        chromeDriver.findElement(By.id("dropdownMenuButton")).click();
-        chromeDriver.findElement(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + (3) + "]")).click();
-        String actualResult3 = chromeDriver.getCurrentUrl();
-        String expectedResult3 = expectedLinks[2];
-        Assert.assertEquals(actualResult3, expectedResult3);
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + index + "]")));
+        chromeDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        chromeDriver.findElement(By.xpath("//button[@id=\"dropdownMenuButton\"]/following-sibling::div/a[" + index + "]")).click();
+        String actualResult = chromeDriver.getCurrentUrl();
+        String expectedResult = expectedLink;
+        Assert.assertEquals(actualResult, expectedResult);
         chromeDriver.navigate().back();
     }
 }
